db11e29e201c34d8d68799ee2918a12e5fd2919e
hieukhocnhe <trantrunghieu422@gmail.com>
2024-01-08 23:42:33 +0700

    Lesson 112 done

---
+++ lessons/lesson_112.js
@@ -0,0 +1,46 @@
+/**
+ * Mon Jan 08
+ * Lesson 112 : Enhanced Object Literals
+ * 
+ */
+
+const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[6]]: {
+        open: 0, // Open 24 hours 
+        close: 24
+    },
+};
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Brushetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours,
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery({ starterIndex, mainIndex, address, time }) {
+        console.log(`Order delivered ! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+    orderPasta(ing1, ing2, ing3) {
+        console.log(`Here is ur declicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+    },
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+console.log(restaurant.openingHours);
